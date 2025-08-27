860566f88c8486940910e219ec7d927ecefbdb05
HironobuOmura <124325573+HironobuOmura@users.noreply.github.com>
2025-08-27 11:19:07 +0900

    javaファイル
    javaファイル追加しました

---
+++ kadai_15/Car_Exec_Chapter15.java
@@ -0,0 +1,12 @@
+package kadai_15;
+
+public class Car_Exec_Chapter15 {
+
+	public static void main(String[] args) {
+		Car_Chapter15 car= new Car_Chapter15(3);
+		
+		car.changeGear(3);
+		car.run(3);
+	}
+
+}
