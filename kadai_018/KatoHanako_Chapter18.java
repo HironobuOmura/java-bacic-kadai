a5353991aa17f22d80bc32a437b6dade1c9481d5
HironobuOmura <124325573+HironobuOmura@users.noreply.github.com>
2025-08-29 10:22:06 +0900

    a
    a

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,13 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18{
+
+	public void setGivenName() {
+		givenName ="花子";
+		System.out.println("名前は加藤" + givenName + "です");
+	}
+    
+	void eachInstroduce() {
+	      System.out.println("趣味は読書です");
+	}
+}
