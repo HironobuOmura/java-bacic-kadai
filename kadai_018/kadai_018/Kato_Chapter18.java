351b9e58f33a1dd89e0155499a8bd55616fb947e
HironobuOmura <124325573+HironobuOmura@users.noreply.github.com>
2025-09-01 12:00:20 +0900

    再提出

---
+++ kadai_018/kadai_018/Kato_Chapter18.java
@@ -0,0 +1,20 @@
+package kadai_018;
+
+abstract public class Kato_Chapter18 {
+	public String familyName;
+	public String givenName;
+	public String address = "東京都中野区〇×";
+	
+
+	abstract void eachIntroduce();
+	
+	public void commonIntroduce() {
+	    System.out.println("住所は" +address+ "です");
+	}
+	
+	public void execIntroduce() {
+		commonIntroduce();
+		eachIntroduce();
+	}
+
+}
