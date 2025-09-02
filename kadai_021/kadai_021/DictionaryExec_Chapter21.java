9f2cc019ca289441ed9a23379531cd96c7963da1
HironobuOmura <124325573+HironobuOmura@users.noreply.github.com>
2025-09-02 09:25:10 +0900

    reup
    reup

---
+++ kadai_021/kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,16 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 extends Dictionary_Chapter21 {
+
+	public static void main(String[] args) {
+		
+
+		String[] words = {"apple", "banana", "grape", "orange"};
+
+		
+		System.out.println(words);
+		
+	}
+				
+
+}
