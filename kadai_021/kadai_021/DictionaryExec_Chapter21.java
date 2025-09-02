a6842b6e4d8fff21344f1274932a0a0f1df10019
HironobuOmura <124325573+HironobuOmura@users.noreply.github.com>
2025-09-02 09:40:41 +0900

    あ
    あ

---
+++ kadai_021/kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,18 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21{
+
+	public static void main(String[] args) {
+		Dictionary_Chapter21 dict = new Dictionary_Chapter21();
+
+		String[] words = new String[] {"apple", "banana", "grape", "orange"};
+
+		
+		dict.getMeanings(words);
+		
+		System.out.println(words);
+		
+	}
+				
+
+}
